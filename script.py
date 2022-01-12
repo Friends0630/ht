186b86beddfc2970df1bc63563c1c63b1c06d4a9
Friends0630 <97385506+Friends0630@users.noreply.github.com>
2022-01-12 11:55:10 +0900

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
 import streamlit as st
 money=5000
-def main():
+while(True):
+  e=1
   st.title('丁半')
   th= st.radio('偶数だと思うなら丁、奇数だと思うなら半を入力',['丁', '半'])
-  money0 = st.slider(label='賭け金を入力(現在の所持金:'+str(money)+"円)",min_value=0,max_value=money,value=0)
-if __name__ == '__main__':
-    main()
+  while e==0:
+    st.number_input(label='賭け金を入力(現在の所持金:'+str(money)+"円)",value=0)
+    if money < money0:
+      st.error('所持金を超える賭け金は賭けられません')
+    else:
+      e=0
