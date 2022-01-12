71c3f6e8aec89362ceca1adf50db53e61c6fdd9b
Friends0630 <97385506+Friends0630@users.noreply.github.com>
2022-01-12 11:41:12 +0900

    Update script.py

--- script.py
+++ script.py
@@ -3,5 +3,10 @@ money=5000
 def main():
   st.title('丁半')
   th= st.radio('偶数だと思うなら丁、奇数だと思うなら半を入力',['丁', '半'])
+    money0 = st.slider(label='賭け金を入力(現在の所持金:'+str(money)+"円)",
+                    min_value=0,
+                    max_value=money,
+                    value=0,
+                    )
 if __name__ == '__main__':
     main()
