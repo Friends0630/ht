e11a6f81731a98ef8b8877068490f60212cc3e4d
Friends0630 <97385506+Friends0630@users.noreply.github.com>
2022-01-13 09:08:18 +0900

    Update script.py

--- script.py
+++ script.py
@@ -2,8 +2,7 @@ import streamlit as st
 money=5000
 money=0
 st.title('丁半')
-while(True):
-  e=1
+
   th= st.radio('偶数だと思うなら丁、奇数だと思うなら半を入力',['丁', '半'])
   st.number_input(label='賭け金を入力(現在の所持金:'+str(money)+"円)",value=0)
   if st.button('決定'):
