e13fc61fe72d487e2db438d52991ac38659d1902
Friends0630 <97385506+Friends0630@users.noreply.github.com>
2022-01-13 09:02:23 +0900

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ while(True):
   e=1
   th= st.radio('偶数だと思うなら丁、奇数だと思うなら半を入力',['丁', '半'])
   st.number_input(label='賭け金を入力(現在の所持金:'+str(money)+"円)",value=0)
-  if money < money0:
+  if money => money0:
     st.error('所持金を超える賭け金は賭けられません')
   else:
     e=0
